9c0a3bf33f256e9464ed2b66526d7de75a88ab18
Patrick Seeman <patseem189@gmail.com>
2026-06-08 23:01:30 -0400

    render integers and strings properly

--- SQLTests.py
+++ SQLTests.py
@@ -3,6 +3,7 @@ from py_sql_toolkit import SQLBuilder
 
 
 def main():
+    print("Generating query with WHERE IN filter using strings")
     mrn_list = [
         "MRN123",
         "MRN456",
@@ -26,6 +27,33 @@ def main():
             assert mrn in query
 
         sqlglot.parse(query, dialect="snowflake", error_level=sqlglot.ErrorLevel.RAISE)
+        print(query)
+
+    print("Generating query with WHERE IN filter using numbers")
+    mrn_list_numbers = [
+        123,
+        456,
+        789,
+        101112,
+        131415,
+        161718,
+        192021,
+        222324,
+        252627,
+        282930,
+    ]
+
+    with open("PathologyExtract.sql", "r") as f:
+        query = f.read()
+        query = SQLBuilder.add_in_filter(
+            query, "PAT_MRN_ID", mrn_list_numbers, dialect="snowflake", pretty=True
+        )
+
+        for mrn in mrn_list_numbers:
+            assert mrn in query
+
+        sqlglot.parse(query, dialect="snowflake", error_level=sqlglot.ErrorLevel.RAISE)
+        print(query)
 
     print("No exceptions raised!")
 
